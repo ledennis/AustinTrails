a7ab8d1fb241c08fb4389454addde31644e9a361
ledennis <dennisle@wireless-10-146-195-80.public.utexas.edu>
2016-04-25 16:16:19 -0500

    Legend

--- js/trailCalls.js
+++ js/trailCalls.js
@@ -108,33 +108,33 @@ app.controller('ctrl', function($scope, $http) {
           strokeColor: '#006600'
         });
 
-        var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-        var icons = {
-          parking: {
-            name: 'Parking',
-            icon: iconBase + 'parking_lot_maps.png'
-          },
-          library: {
-            name: 'Library',
-            icon: iconBase + 'library_maps.png'
-          },
-          info: {
-            name: 'Info',
-            icon: iconBase + 'info-i_maps.png'
-          }
-        };
+        // var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+        // var icons = {
+        //   parking: {
+        //     name: 'Parking',
+        //     icon: iconBase + 'parking_lot_maps.png'
+        //   },
+        //   library: {
+        //     name: 'Library',
+        //     icon: iconBase + 'library_maps.png'
+        //   },
+        //   info: {
+        //     name: 'Info',
+        //     icon: iconBase + 'info-i_maps.png'
+        //   }
+        // };
 
-        var legend = document.getElementById('legend');
-        for (var key in icons) {
-          var type = icons[key];
-          var name = type.name;
-          var icon = type.icon;
-          var div = document.createElement('div');
-          div.innerHTML = '<img src="' + icon + '"> ' + name;
-          legend.appendChild(div);
-        }
+        // var legend = document.getElementById('legend');
+        // for (var key in icons) {
+        //   var type = icons[key];
+        //   var name = type.name;
+        //   var icon = type.icon;
+        //   var div = document.createElement('div');
+        //   div.innerHTML = '<img src="' + icon + '"> ' + name;
+        //   legend.appendChild(div);
+        // }
 
-        map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
+        // map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
     }
     google.maps.event.addDomListener(window, 'load', initMap);
 
